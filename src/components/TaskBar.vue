187958a039667959161501a0c1e64bc2bccaa87a
PamiFalade <jesupamilerin.falade@ucalgary.ca>
2024-06-08 16:48:31 -0600

    Added Task bar

---
+++ src/components/TaskBar.vue
@@ -0,0 +1,129 @@
+<template>
+
+    <div id="mainTaskBar">
+        <div id="taskBarComponents">
+            <div class="imgBtn">
+                <img src="../assets/clear_icon.png"/>
+            </div>
+
+            <h2>{{ title }}</h2>
+
+
+            <div class="dropdown">
+                <button class="dropbtn">Export</button>
+                <div class="dropdown-content">
+                    <a href="#">PNG</a>
+                    <a href="#">PDF</a>
+                    <a href="#">Excel</a>
+                </div>
+            </div>
+
+            <div class="imgBtn">
+                <img class="imgBtn" src="../assets/save_icon.png"/>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+
+<script>
+
+export default {
+    name: 'TaskBar',
+    props: {
+        title: String
+    }
+}
+
+</script>
+
+<style>
+#mainTaskBar{
+    width: 95vw;
+    height: 8vh;
+    background-color: grey;
+    padding: 2vh 3vw;
+    align-items: center;
+}
+
+#taskBarComponents {
+    display: grid;
+    grid-template-columns: 1fr 5fr 1fr 1fr;
+    column-gap: 2vw;
+    grid-template-rows: 1fr;
+    justify-content: center;
+    align-items: center;
+}
+
+.imgBtn {
+    width: 50%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+ }
+.imgBtn img {
+    max-width: 30px;
+    max-height: 30px;
+}
+.imgBtn:hover {
+    cursor: pointer;
+    background-color: white;
+    border-radius: 50%;
+}
+
+.middle {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.rightSide {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+/* Styling for export dropdown menu */
+.dropbtn {
+  background-color: #04AA6D;
+  color: white;
+  padding: 16px;
+  font-size: 16px;
+  border: none;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f1f1f1;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content a:hover {background-color: #ddd;}
+
+.dropdown:hover .dropdown-content {display: block;}
+
+.dropdown:hover .dropbtn {background-color: #3e8e41;}
+/* End styling for export dropdown */
+
+</style>
